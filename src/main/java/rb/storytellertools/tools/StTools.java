2944fbdeb2031479c5d4c7110fbc8c12ba496ce5
Robert Beer <rjbeer08@gmail.com>
2026-05-30 01:09:51 -0400

    simple tokenizer added

--- src/main/java/rb/storytellertools/tools/StTools.java
+++ src/main/java/rb/storytellertools/tools/StTools.java
@@ -1,6 +1,7 @@
 package rb.storytellertools.tools;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StTools{
     
@@ -9,12 +10,21 @@ public class StTools{
 	Game game = new Game();
 	String prompt = "stt: ";
 	String userIn = "";
-	
-	while(!(userIn.equals("quit"))){
+	String curCmd = "";
+	Tokenizer t = new Tokenizer();
+	ArrayList<String> arguments = new ArrayList<>();
+
+	while(!(curCmd.equals("quit"))){
 		System.out.print(prompt);
 		userIn = sc.nextLine();
+		arguments = t.parseArgs(userIn);
+		curCmd = arguments.get(0);
 
-		switch(userIn){
+		for(int i = 0; i < arguments.size(); i++){
+			System.out.println(arguments.get(i));
+		}
+
+		switch(curCmd){
 			case "new":
 				System.out.print("\n\n\nGame Name: ");
 				userIn = sc.nextLine();
---
+++ src/main/java/rb/storytellertools/tools/Tokenizer.java
@@ -0,0 +1,29 @@
+package rb.storytellertools.tools;
+
+import java.util.ArrayList;
+
+public class Tokenizer {
+	
+
+	public ArrayList<String> parseArgs(String cmd){
+		ArrayList<String> parsedArgs = new ArrayList<>();
+		StringBuilder argument = new StringBuilder();
+		char[] pieces = cmd.toCharArray();
+		boolean doubleQuote = false;
+
+		for(int i = 0; i < pieces.length; i++){
+			if(pieces[i] == ' '){
+				parsedArgs.add(argument.toString());
+				argument = new StringBuilder();
+			} else {
+				argument.append(pieces[i]);
+			}
+		}
+		
+		if(argument.length() > 0){
+			parsedArgs.add(argument.toString());
+		}
+
+		return parsedArgs;
+	}
+}
